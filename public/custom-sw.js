13c4b43aa6d41cb1a7b7951438e294928d2f453a
raysoju <raymond.lim672@gmail.com>
2020-12-10 16:55:07 +0100

    error maybe

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,9 +1,9 @@
 self.addEventListener('push', (event) => {
   const data = event.data.json();
-  console.log('New notification', data);
-  const options = {
+  const { title } = data;
+  const body = {
     body: data.body,
     title: data.title,
   };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+  event.waitUntil(self.registration.showNotification(title, body));
 });
