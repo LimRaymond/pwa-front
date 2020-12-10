42094cdc381d61c764f3fc556d73579a39ddf600
raysoju <raymond.lim672@gmail.com>
2020-12-10 15:37:57 +0100

    notification push

---
+++ public/custom-sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  console.log('New notification', data);
+  const options = {
+    body: data.body,
+    title: data.title,
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { subscribeUser } from './subscription';
 import store from './store/store';
 
 ReactDOM.render(
@@ -12,10 +13,12 @@ ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>, document.getElementById('root'),
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+subscribeUser();
